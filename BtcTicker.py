f6f711fea1fb0692a187bfd036f8c3add6197283
zantelope <42042206+zantelope@users.noreply.github.com>
2018-09-13 16:29:35 -0500

    Create BtcTicker.py

---
+++ BtcTicker.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import threading
+
+def setInterval(func,time):
+    e = threading.Event()
+    while not e.wait(time):
+        func()
+
+
+class BtcObject(object):
+  def __init__(self, dictr):
+    self.dictr = dictr
+    self.time = dictr["time"]["updated"]
+    self.price = dictr["bpi"]["USD"]["rate"]
+
+
+
+def foo():
+  r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+  test_object = BtcObject(r.json())
+  print(test_object.time)
+  print(test_object.price)
+
+
+setInterval(foo, 5)
